rzr/webthings-webapp

app: Add put callback for switch
Change-Id: I437d9dbf0f4ca849c234071393f54561fc3c95df
Signed-off-by: Philippe Coval <p.coval@samsung.com>

js/index.js
@@ -86,6 +86,26 @@ function request()
   query();
 }
 
+function put(endpoint, payload, callback)
+{
+
+  var url = window.form.url.value + endpoint;
+  var token = localStorage['token'];
+  payload = JSON.stringify(payload);
+  log(url); //TODO
+  log(payload);
+  var request = new XMLHttpRequest();
+  request.addEventListener('load', function() {
+    callback = callback || {};
+    callback(null, this.responseText);
+  });
+    request.open('PUT', url);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.setRequestHeader('Accept', 'application/json');
+  request.setRequestHeader('Authorization', 'Bearer ' + token);
+  request.send(payload);
+}
+
 function updateView(model, view)
 {
   var self = this;
@@ -117,6 +137,22 @@ function createViewOnOffSwitch(li, model)
   radio.setAttribute('aria-disabled', "false");
   radio.setAttribute('data-tau-bound', "ToggleSwitch");
   var endpoint = model.properties.on.href;
+  radio.local = {};
+  radio.addEventListener('click', function(){
+    radio.disabled  = true;
+    var wanted = (this.checked);
+    radio.local.interval = setTimeout(function(){
+      radio.disabled = false;
+    }, 1000);
+ 
+    var self = this;
+    log('wanted: ' + wanted);
+    put(endpoint, { on: wanted }, function(res, data) {
+      self.checked  = !! (JSON.parse(data).on)
+      clearInterval(radio.local.interval);
+      radio.disabled = false;
+    });
+  });
   div.appendChild(radio);
   var handlerdiv = document.createElement('div');
   handlerdiv.setAttribute('class', 'ui-switch-handler');

TizenTeam/tizenteam.github.io

js/index.js
@@ -128,10 +128,8 @@ app.updateView = function(model, view)
     this.get(endpoint, function(err, data) {
       view.local.widget.checked = !!(JSON.parse(data).on);
     });
-  } else if (model.type == "multilevelSensor") {
-    this.get(model.properties.level.href, function(err, data) {
-      view.local.button.innerText = JSON.parse(data).level;
-    });
+  } else {
+    console.log("TODO: implement " + model.type);
   }
 };
 
@@ -168,6 +166,7 @@ app.createBinarySensorView = function(li, model)
 
 app.createOnOffSwitchView = function(li, model)
 {
+  var self = this;
   li.setAttribute('class', 'ui-li-static ui-li-1line-btn1');
   var div = document.createElement('div');
   div.setAttribute('class', 'ui-btn.ui-btn-box-s ui-toggle-container');
@@ -181,6 +180,21 @@ app.createOnOffSwitchView = function(li, model)
   widget.setAttribute('aria-disabled', "false");
   widget.setAttribute('data-tau-bound', "ToggleSwitch");
   var endpoint = model.properties.on.href;
+  widget.local = {};
+  widget.addEventListener('click', function(){
+    widget.disabled  = true;
+    var wanted = (this.checked);
+    widget.local.interval = setTimeout(function(){
+      widget.disabled = false;
+    }, 1000);
+ 
+    self.log('wanted: ' + wanted);
+    self.put(endpoint, { on: wanted }, function(res, data) {
+      self.checked  = !! (JSON.parse(data).on)
+      clearInterval(widget.local.interval);
+      widget.disabled = false;
+    });
+  });
   div.appendChild(widget);
   var handlerdiv = document.createElement('div');
   handlerdiv.setAttribute('class', 'ui-switch-handler');